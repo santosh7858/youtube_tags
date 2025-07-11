2aaeeae5905db634cb87b60e54e76702f0a885ea
santosh7858 <137761972+santosh7858@users.noreply.github.com>
2025-07-11 09:57:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template, request
 from tags import generate_tags
+import os
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
-def home():
+def index():
     tags = ""
     if request.method == "POST":
-        input_text = request.form.get("text")
-        if input_text:
-            tags = generate_tags(input_text, max_phrases=15)
+        title = request.form.get("title", "")
+        if title:
+            tags = generate_tags(title)
     return render_template("index.html", tags=tags)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
